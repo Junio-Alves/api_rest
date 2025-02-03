3b7dabd2dc66293ef1bd275168a67f247660fc51
Junio-Alves <juniophb2004@gmail.com>
2025-02-03 11:55:14 -0300

    177 e 178 - Gerando JWT dos usuários e Middleware de Verificação de JWT.

---
+++ src/controllers/tokenController.js
@@ -0,0 +1,35 @@
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+require("dotenv").config();
+class TokenController {
+    async store(req,res){
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                errors: ["Invalid credentials"],
+            })
+        }
+
+        const user = await User.findOne({where: { email }});
+
+        if(!user){
+            return res.status(400).json({
+                errors: ["Usuário não existe"],
+            })
+        }
+
+        if(!(await user.passwordIsValid(password))){
+            return res.status(400).json({
+                errors: ["Senha Invalida"],
+            })
+        }
+        const { id } = user;
+        const token = jwt.sign({id,email},process.env.TOKEN_SECRET,{
+            expiresIn: process.env.TOKEN_EXPIRATION,
+        })
+
+        res.json({token});
+    }
+}
+
+export default new TokenController();
