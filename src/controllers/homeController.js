25561cd48e96b1df775568582b739068d0a72752
Junio-Alves <juniophb2004@gmail.com>
2025-02-04 11:48:06 -0300

    181: Rotas de Alunos

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,6 @@
-import Aluno from "../models/Aluno";
 class HomeController {
     async index(req,res){
-        const novoAluno = await Aluno.create({
-            nome:"Junio",
-            sobrenome: "Alves",
-            email: "juniophb2004@gmail.com",
-            idade: 21,
-            peso: 60,
-            altura: 1.70,
-        }); 
-        res.status(200).json(novoAluno);
+        res.json(novoAluno);
     }
 }
 
