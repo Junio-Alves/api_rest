be3341249afe4c0274c8df3530979c91bbd01c44
Junio-Alves <juniophb2004@gmail.com>
2025-02-05 20:24:48 -0300

    202- Modificações na API REST

--- src/app.js
+++ src/app.js
@@ -4,11 +4,28 @@ import {resolve} from "path";
 dotenv.config();
 import "./database";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
 import homeRoutes from "./routes/homeRoutes";
 import userRoutes from "./routes/userRoutes";
 import tokenRoutes from "./routes/tokenRoutes";
 import alunoRoutes from "./routes/alunoRoutes";
 import fotoRoutes from "./routes/fotoRoutes";
+
+const whitelist = [
+    "http://localhost:3002",
+];
+
+const corsOptions = {
+    origin: function(origin,callback){
+        if(whitelist.indexOf(origin) != -1 || !origin){
+            callback(null,true);
+        }else{
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}
 
 class App{
     constructor(){
@@ -19,6 +36,8 @@ class App{
     }
 
     middlewares(){
+        this.app.use(cors(corsOptions));
+        this.app.use(helmet());
         this.app.use(express.urlencoded({extended: true}));
         this.app.use(express.json());
         this.app.use("/images",express.static(resolve(__dirname,"..","uploads","images")));
