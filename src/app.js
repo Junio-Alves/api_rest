28de6807fe0740a978bbb0b789a21cbbbe2b29ea
Junio-Alves <juniophb2004@gmail.com>
2025-02-10 10:30:25 -0300

    Cors added again

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 import dotenv from "dotenv";
 import {resolve} from "path";
-
+import cors from "cors";
 dotenv.config();
 import "./database";
 import express from "express";
 import helmet from "helmet";
-
 import homeRoutes from "./routes/homeRoutes";
 import userRoutes from "./routes/userRoutes";
 import tokenRoutes from "./routes/tokenRoutes";
 import alunoRoutes from "./routes/alunoRoutes";
 import fotoRoutes from "./routes/fotoRoutes";
 
+const whiteList = [
+    'http://localhost:3002',
+    'http://localhost:3001',
+    'http://localhost:3000',
+    '*',
+  ];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if(whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
 
 class App{
     constructor(){
@@ -22,6 +37,7 @@ class App{
     }
 
     middlewares(){
+        this.app.use(cors(corsOptions));
         this.app.use(helmet());
         this.app.use(express.urlencoded({extended: true}));
         this.app.use(express.json());
